ad3e1e681c7168e59292cc1cf064c4821ccfc70e
Sathish <satissh12@gmail.com>
2021-06-19 12:54:11 +0530

    my second commit

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,5 +1,19 @@
 # merge two array and print them 
 
+#3Initlaization 
+#Create on Web UI 
+# initalize local 
+# remote 
+
+# Pushing 
+
+#Add Files we want to send ( staging the changes )
+# commit ( Commiting the changes )
+#push 
+
+
+
+
 
 a=[]
 
