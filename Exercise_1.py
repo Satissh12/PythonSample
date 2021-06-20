0b5ac0eae83c4942e97f518bbd1946a02b486007
Sathish <satissh12@gmail.com>
2021-06-20 11:36:00 +0530

    git commit Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,23 +1,29 @@
-# merge two array and print them 
+# merge two array and print them
 
-#3Initlaization 
-#Create on Web UI 
-# initalize local 
-# remote 
+# 3Initlaization
+# Create on Web UI
+# initalize local
+# remote
 
-# Pushing 
+# Pushing
 
-#Add Files we want to send ( staging the changes )
+# Add Files we want to send ( staging the changes )
 # commit ( Commiting the changes )
-#push 
+# push git push origin <branch>
 
 
 
+def mergearray(arr1, arr2):
+    a = arr1
+    b = arr2
+    c = a+b
+    c.sort()
+    print(c)
+    c.sort(reverse=True)
+    print(c)
+    for items in c:
+        # range (0,c.__len__()):
+        print (items)
 
 
-a=[]
-
-def mergearray(arr1,arr2):
-    pass
-
-mergearray()
+mergearray([7, 8, 9],[2, 5, 6])
